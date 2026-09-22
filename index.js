913ee03c8e331e1cc744d9f36479eb90efbb4e74
halalmhuhu-hue <halalmhuhu@gmail.com>
2026-09-22 12:27:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Example baseHost at host is set up to respond with HTML
  * Replace url with the host you wish to send requests to
  */
-const baseHost = 'https://tiktok.com//'
+const baseHost = 'https://agar.io//'
 
 /**
  * gatherResponse awaits and returns a response body with appropriate headers.
